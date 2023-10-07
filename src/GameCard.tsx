8275eb3f6bf42e6a699a1bef4f86494d1e1a114a
Desmond51 <desmondtekum9@gmail.com>
2023-10-07 14:32:39 +0100

    Created a component

---
+++ src/GameCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Game } from './hooks/useGames'
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+
+interface Props{
+    game:Game[];
+}
+export default function GameCard({game}:Props) {
+  return (
+    <Card>
+        <Image src={game.background_image} />
+        <CardBody>
+            <Heading>{game.name}</Heading>
+        </CardBody>
+    </Card>
+  )
+}
