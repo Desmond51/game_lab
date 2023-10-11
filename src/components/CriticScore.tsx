82c02aa826468055edb50d03b790a0314d7c81a8
Desmond51 <desmondtekum9@gmail.com>
2023-10-11 04:46:35 +0100

    Added color variation to the Metacritics

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@chakra-ui/react";
+
+
+interface Props{
+    score: number;
+}
+export default function CriticScore({score}:Props) {
+    let color = score>75 ? "green": score > 60 ? "yellow" : ""; 
+  return (
+    <Badge fontSize= "14px" paddingX={2} borderRadius='5px' colorScheme={color}>{score}</Badge>
+  )
+}
